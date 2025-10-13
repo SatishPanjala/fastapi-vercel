91de4b43464368f8c523769235a547411e92b089
Satish.Panjala <Satish.Panjala@elysiumanalytics.ai>
2025-10-13 19:06:02 +0530

    sample apis

---
+++ api/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI(title="FastAPI on Vercel - All Methods Example")
+
+@app.get("/user")
+def get_user():
+    return JSONResponse({"message": "User Returned"})
+
+@app.post("/user")
+def create_user():
+    return JSONResponse({"message": "User Created"})
+
+@app.put("/user")
+def update_user():
+    return JSONResponse({"message": "User Updated"})
+
+@app.delete("/user")
+def delete_user():
+    return JSONResponse({"message": "User Deleted"})
